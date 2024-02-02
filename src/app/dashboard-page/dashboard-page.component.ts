11bbe4dc6b0e0203810b69247d555f9c88beb9c5
agboola teslim <teslimagboola09@gmail.com>
2024-02-02 12:56:33 +0100

    repsug:

--- src/app/dashboard-page/dashboard-page.component.ts
+++ src/app/dashboard-page/dashboard-page.component.ts
@@ -1,18 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './dashboard-page.component.html',
   styleUrl: './dashboard-page.component.css'
 })
 export class DashboardPageComponent implements OnInit {
   stustudentArray: any[] = [];
   ngOnInit() {
-    const stustoredUsers = localStorage.getItem('stustudents');
-    if (stustoredUsers) {
-      this.stustudentArray = JSON.parse(stustoredUsers);
+    const stustoredUsers = JSON.parse(localStorage['stustudents'] )
+      if (stustoredUsers) {
+      this.stustudentArray = stustoredUsers
+      console.log(this.stustudentArray );
+      
     }
   }
 }
