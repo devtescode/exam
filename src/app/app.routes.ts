83aa24e7295e0253d46c5b3a509cee13c51782fd
agboola teslim <teslimagboola09@gmail.com>
2024-02-02 14:18:53 +0100

    repush

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,17 +4,17 @@ import { SigninPageComponent } from './signin-page/signin-page.component';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 import { StudentPageComponent } from './student-page/student-page.component';
 import { LandingpageComponent } from './landingpage/landingpage.component';
-// import { adminGuardGuard } from './guards/admin-guard.guard';
+import { adminGuardGuard } from './guards/admin-guard.guard';
 
 export const routes: Routes = [
     {path: '', component:LandingpageComponent},
     {path: 'signup', component: SignupPageComponent},
     {path: 'signin', component: SigninPageComponent},
-    {path: 'dashboard', component: DashboardPageComponent},
+    // {path: 'dashboard', component: DashboardPageComponent},
     {path: 'student', component: StudentPageComponent},
-    // {path: 'dashboard', children:[
-    //     {path: '', component: DashboardPageComponent},
-    // ],canActivate:[adminGuardGuard]},
+    {path: 'dashboard', children:[
+        {path: '', component: DashboardPageComponent},
+    ],canActivate:[adminGuardGuard]},
 ];
 
 
